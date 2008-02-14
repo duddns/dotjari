a9fee5f824a1e482285b8fa7de657a88a2b3e56e
duddns <duddns@6229dff6-0346-0410-b06f-47c212ff59f9>
2008-02-14 12:30:27 +0000

    * 화면에 표시되는 정보 수정    * 공고명, 회사, 지역, 채용방식, 모집 마감일  * 리스트 마크업 수정    * ul -> dl  * 페이징 기능 추가    * 위젯 속성에 화면에 표시되는 갯수 추가    * api 쿼리에 count, start 파라미터 추가

--- test.xml.php
+++ test.xml.php
@@ -1,7 +1,7 @@
 <?php
 
 extract($_GET); 
-$url = "http://cxiol.incruit.com/api/?method=search&q=$q&apikey=A45F959AEA04D2799C4C1824DF101FE7FC0EB507&out=xml";
+$url = "http://cxiol.incruit.com/api/?method=search&q=$q&start=$start&count=$count&apikey=A45F959AEA04D2799C4C1824DF101FE7FC0EB507&out=xml";
 
 header ("content-type: text/xml");
 echo getHtml($url); 
