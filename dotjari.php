f33c61044e423c4b8498939eee8dbaf0cc93e9cc
duddns <duddns@6229dff6-0346-0410-b06f-47c212ff59f9>
2008-03-21 09:45:59 +0000

    * 씨올 BI 수정

--- dotjari.php
+++ dotjari.php
@@ -1,6 +1,6 @@
 <?php
 extract($_GET); 
-$url = "http://cxiol.incruit.com/api/?method=search&apikey=A45F959AEA04D2799C4C1824DF101FE7FC0EB507&out=xml";
+$url = "http://naeilapi.incruit.com/api/?method=search&apikey=A45F959AEA04D2799C4C1824DF101FE7FC0EB507&out=xml";
 $q = rawurlencode($q);
 $url .= "&q=$q&start=$start&count=$count";
 if (0 < strlen($rgn1)) {
